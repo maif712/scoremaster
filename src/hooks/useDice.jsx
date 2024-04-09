898622a0ee198d9683c0b592cb0c8b3b79b80b91
maif712 <maif221@gmail.com>
2024-04-09 22:58:06 +0330

    added dice roll history

--- src/hooks/useDice.jsx
+++ src/hooks/useDice.jsx
@@ -4,20 +4,30 @@ import { diceData } from "../constants";
 export default function useDice() {
 
     const [dices, setDices] = useState([])
+    const [rollsHistory, setRollsHistory] = useState([])
+    const [sixAmount, setSixAmount] = useState(0)
     const [chosenItemIndex, setChosenItemIndex] = useState(2)
     const [isRolling, setIsRolling] = useState(false)
     const [rnd, setRnd] = useState(false)
+
+    const transitionItming = 400;
 
     useEffect(() => {
         setDices(diceData.sort(() => 0.5 - Math.random()))
     }, [rnd])
 
+    
+    useEffect(() => {
+        const includeSixArray = rollsHistory.filter(roll => roll.value == 6)
+        setSixAmount(includeSixArray.length)
+    }, [rollsHistory])
+   
+
 
     let temp = 0
     const RandomNumber = () => {
         const rndNumber = Math.floor(Math.random() * diceData.length)
-        if(temp == rndNumber)
-        {
+        if (temp == rndNumber) {
             return RandomNumber()
         }
         else {
@@ -28,23 +38,34 @@ export default function useDice() {
 
     const handleRoll = () => {
 
+        let getRndNumber;
+
         const finallTime = 1200
         setRnd(prev => !prev)
         setIsRolling(true)
         for (let index = 0; index <= diceData.length; index++) {
             setTimeout(() => {
-                setChosenItemIndex(RandomNumber())
+                getRndNumber = RandomNumber()
+
+                setChosenItemIndex(getRndNumber)
+                setTimeout(() => {
+                    if(index == diceData.length){ 
+                        setRollsHistory(prev => [dices[getRndNumber], ...prev])
+                    }
+                }, transitionItming);
             }, (finallTime / diceData.length) * index);
         }
         setTimeout(() => {
             setIsRolling(false)
-        }, finallTime + 400); //400 is for 0.4s dices container transitiom timing
+        }, finallTime + transitionItming); //400 is for 0.4s dices container transitiom timing
     }
 
     return {
         dices,
         isRolling,
-        handleRoll, 
-        chosenItemIndex
+        handleRoll,
+        chosenItemIndex,
+        rollsHistory,
+        sixAmount
     }
 }
--- src/pages/Dice.jsx
+++ src/pages/Dice.jsx
@@ -9,7 +9,9 @@ export default function Dice() {
     dices,
     isRolling,
     handleRoll,
-    chosenItemIndex
+    chosenItemIndex,
+    rollsHistory,
+    sixAmount
   } = useDice()
 
   // const DiceComponent = selectedDice?.el
@@ -20,7 +22,7 @@ export default function Dice() {
         <h2 className="dice-title">roll-a-dice</h2>
         <div className="flex flex-col items-center gap-1 mt-5 mb-full">
           <div className="dices-parent overflow-hidden">
-            <div style={{"--x": `-${(chosenItemIndex) * 116}%`}} className="dices-container">
+            <div style={{ "--x": `-${(chosenItemIndex) * 116}%` }} className="dices-container">
               {/* <DiceComponent /> */}
               {dices.map(dice => {
                 return (
@@ -32,6 +34,21 @@ export default function Dice() {
             </div>
           </div>
           <button onClick={handleRoll} className={`roll-dice-btn ${isRolling ? "disable" : ""}`}>roll the dice</button>
+          <div className="rolls-history">
+            <div className="sticky">
+              <h3 className="rolls-history__title">rolls history</h3>
+              <p className="rolls-history__para">your six: ({sixAmount})</p>
+            </div>
+            <div className="flex flex-col gap-05 p-block-1">
+              {rollsHistory.map((roll, index) => {
+                return (
+                  <div key={index} className={`roll ${roll.value == 6 ? "six" : ""}`}>
+                    <span>{roll.value}</span>
+                  </div>
+                )
+              })}
+            </div>
+          </div>
         </div>
       </div>
     </section>
